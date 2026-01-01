c08d0705e5c54ada264a95ff461f59a6bf53e198
GengWei1997 <1638044635@qq.com>
2026-01-01 19:02:54 +0800

    6

--- debian-desktop_build.sh
+++ debian-desktop_build.sh
@@ -42,7 +42,10 @@ chroot rootdir apt update
 chroot rootdir apt upgrade -y
 
 # 安装基础软件包
-chroot rootdir apt install -y bash-completion sudo apt-utils ssh openssh-server nano systemd-boot initramfs-tools chrony curl wget u-boot-tools $1
+chroot rootdir apt install -y bash-completion sudo apt-utils ssh openssh-server nano systemd-boot initramfs-tools chrony curl wget u-boot-tools
+
+# 安装桌面环境
+chroot rootdir apt install -y phosh phosh-mobile-tweaks phosh-plugins phoc squeekboard feedbackd iio-sensor-proxy network-manager-gnome gdm3 gdm3-themes
 
 # 安装设备特定软件包
 chroot rootdir apt install -y rmtfs protection-domain-mapper tqftpserv
